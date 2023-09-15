3f4b37c55d9c2a74684d5e89f1e75b673499e56b
EgrMed-NJU <8056085+EgrMed-NJU@users.noreply.github.com>
2023-09-15 22:52:58 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Homepage of EgrMed Group@NJU
---
+++ header-news.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('header-news').innerHTML = `
+  <div>
+    <div>
+      <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+        <tr bgcolor="#B1040E">
+          <td height="10" colspan="2"></td>
+        </tr>
+        <tr>
+          <td width="5%" bgcolor="#B1040E"></td>
+          <td valign="baseline" bgcolor="#B1040E" style="text-align: left; font-size: 24px; font-family: Georgia, 'Times New Roman', Times, serif; color: #FFFFFF;">
+            <p><strong>南京大学工程医学研究组</strong> (EgrMed Group@NJU)</p>
+            <p>Engineering Medicine Research Group, Nanjing University</p>
+          </td>
+        </tr>
+        <tr bgcolor="#B1040E">
+          <td height="10" colspan="4"></td>
+        </tr>
+      </table>
+    </div>
+    <table width="100%" border="0" cellpadding="0" cellspacing="0">
+	  <tr>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+	  </tr>
+	  <tr>
+		<td width="10%" bgcolor="#F2F1EB" style="text-align: center">&nbsp;</td>
+		<td width="20%" bgcolor="#F2F1EB" style="text-align: center"><span style="color: #000000"><strong><a href="GroupPageColumn/news.html" style="font-size: 24px">研究组新闻</a></strong></span></td>
+		<td width="20%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="GroupPageColumn/progress.html" style="font-size: 24px">研究进展</a></strong></td>
+		<td width="20%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="GroupPageColumn/informal essay.html" style="font-size: 24px">动态与随想</a></strong></td>
+		<td width="20%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="index.html" style="font-size: 24px">返回首页</a></strong></td>
+		<td width="10%" bgcolor="#F2F1EB" style="font-family: '微软雅黑'">&nbsp;</td>
+	  </tr>
+	  <tr>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="15%" height="5" bgcolor="#F2F1EB"></td>
+		<td width="5%" height="5" bgcolor="#F2F1EB"></td>
+	  </tr>
+    </table>
+  </div>
+`;
+});
+// JavaScript Document
