9a4e3ffcf2894b77d74e6b50ac7c0905457b1f75
EgrMed-NJU <8056085+EgrMed-NJU@users.noreply.github.com>
2023-09-15 23:47:18 +0800

    Add files via upload

--- header-main.js
+++ header-main.js
@@ -1,4 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
+  "use strict";
+  document.getElementById('header-main').innerHTML =
+    '<div>' +
+      '<div>' +
+        '<table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">' +
+          '<tr bgcolor="#B1040E">' +
+            '<td height="10" colspan="2"></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td width="5%" bgcolor="#B1040E"></td>' +
+            '<td valign="baseline" bgcolor="#B1040E" style="text-align: left; font-size: 24px; font-family: Georgia, \'Times New Roman\', Times, serif; color: #FFFFFF;">' +
+              '<p><strong>南京大学工程医学研究组</strong> (EgrMed Group@NJU)</p>' +
+              '<p>Engineering Medicine Research Group, Nanjing University</p>' +
+            '</td>' +
+          '</tr>' +
+          '<tr bgcolor="#B1040E">' +
+            '<td height="10" colspan="4"></td>' +
+          '</tr>' +
+        '</table>' +
+      '</div>' +
+      '<div>' +
+        '<table width="100%" border="0" cellspacing="0" cellpadding="0">' +
+          '<tr>' +
+            '<td width="5%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="5%" height="5" bgcolor="#F2F1EB"></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td width="5%" bgcolor="#F2F1EB" style="text-align: center">&nbsp;</td>' +
+            '<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><span style="color: #000000"><strong><a href="index.html" style="font-size: 24px">研究首页</a></strong></span></td>' +
+            '<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="GroupPageColumn/reseaches.html" style="font-size: 24px">研究领域</a></strong></td>' +
+            '<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="GroupPageColumn/members.html" style="font-size: 24px">团队成员</a></strong></td>' +
+            '<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="GroupPageColumn/publications.html" style="font-size: 24px">发表论文</a></strong></td>' +
+            '<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="GroupPageColumn/education.html" style="font-size: 24px">人才培养</a></strong></td>' +
+            '<td width="15%" bgcolor="#F2F1EB" style="text-align: center"><strong><a href="GroupPageColumn/news.html" style="font-size: 24px">新闻速递</a></strong></td>' +
+            '<td width="5%" bgcolor="#F2F1EB" style="font-family: \'微软雅黑\'">&nbsp;</td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td width="5%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="15%" height="5" bgcolor="#F2F1EB"></td>' +
+            '<td width="5%" height="5" bgcolor="#F2F1EB"></td>' +
+          '</tr>' +
+        '</table>' +
+      '</div>' +
+    '</div>';
+});
+
+/*document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('header-main').innerHTML = `
 <div>
   <div>
@@ -55,4 +113,4 @@ document.getElementById('header-main').innerHTML = `
 </div>
 `;
 });
-// JavaScript Document
+// JavaScript Document*/
